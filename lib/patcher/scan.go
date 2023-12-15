cbac968763afd22c35f1452add7d427f85c1dde2
Peter Minten <peter@pminten.nl>
2023-12-15 20:31:28 +0100

    Helper for getting very basic file info
    Just modification time right now.

---
+++ lib/patcher/scan.go
@@ -0,0 +1,37 @@
+package patcher
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+// Limited information about a file.
+type BasicFileInfo struct {
+	// When the file was last modified.
+	ModTime time.Time
+}
+
+// ScanFiles recursively determines all files in the directory
+// and gets limited information such as modification time.
+func ScanFiles(rootDir string) (map[string]BasicFileInfo, error) {
+	infos := make(map[string]BasicFileInfo)
+	filesystem := os.DirFS(rootDir)
+	err := fs.WalkDir(filesystem, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return fmt.Errorf("error while scanning file %q: %w", path, err)
+		}
+		info, err := d.Info()
+		if err != nil {
+			return fmt.Errorf("error while statting file %q: %w", path, err)
+		}
+		infos[filepath.Clean(path)] = BasicFileInfo{ModTime: info.ModTime()}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return infos, nil
+}
